61518529f47989db0cfcc9bba5110c599884b834
numi8462 <yhk8462@naver.com>
2025-01-08 17:25:05 +0900

    Updated line-height on blog page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -42,7 +42,7 @@ const Post = async ({ params }) => {
                     <Sidebar />
                     <article className="prose mt-10 mx-auto font-nanum p-5 popupFromLeft w-[100vw]">
                         <div className="flex flex-col justify-center items-center">
-                            <h1 className="block text-white text-7xl font-fugaz max-md:text-5xl !leading-[4rem]">
+                            <h1 className="block text-white text-7xl font-fugaz max-md:text-5xl !leading-[5rem]">
                                 {post.title}
                             </h1>
 
