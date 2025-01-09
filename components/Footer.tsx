594abf8b7ef32de216818dd4458e1de3444d9b21
numi8462 <yhk8462@naver.com>
2025-01-09 10:10:51 +0900

    Updated sidebar css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
 import { FaLocationArrow } from "react-icons/fa6";
+import Image from "next/image";
+import gitIcon from "@/public/git.svg"
 
 const Footer = () => {
     return (
@@ -11,9 +13,15 @@ const Footer = () => {
                     문의 사항이 있으시면 언제든지 개인 메일로 연락 주시기 바랍니다.
                 </p>
 				<a href="mailto:yhk8462@naver.com" className="flex justify-center items-center gap-2 text-white">메일 보내기 <FaLocationArrow/></a>
-                
             </div>
-            <div className="flex mt-16 md:flex-row flex-col justify-center items-center text-white">
+            
+            <div className="flex justify-center items-center">
+                <a href={"https://github.com/numi8462/"} className="flex justify-start items-center gap-1 mt-5">
+                    <Image src={gitIcon} alt="git icon" width={25}/>
+                </a>
+            </div>
+
+            <div className="flex mt-16 md:flex-row flex-col justify-center items-center text-white-200">
                 <p className="md:text-base text-sm md:font-normal font-light ">
                     © 2024. kimyoungho all rights reserved.
                 </p>
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -13,25 +13,26 @@ const Sidebar = () => {
                 </h1>
             </Link>
             <div className="flex flex-col items-center justify-center mt-10 gap-5">
-                <Image
-                    src={ProfilePic}
-                    alt="profile picture"
-                    width={200}
-                    height={200}
-                />
-                <div className="flex flex-col">
-                    <h1 className="text-2xl font-bold leading-10">@ numi</h1>
+                <div className="relative">
+                    <Image
+                        src={ProfilePic}
+                        alt="profile picture"
+                        width={200}
+                        height={200}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    <a href={"https://github.com/numi8462/"} className="flex justify-start items-center gap-1 hover:text-yellow">
+                        <span className="text-2xl font-bold leading-10">@numi</span>
+                    </a>
+                        
                     <p>
                         안녕하세요, 프론트앤드 개발자
                         <br /> 김영호 입니다.
                     </p>
                 </div>
-                <div className="flex block">
-                    <a href={"https://github.com/numi8462/"} className="flex gap-1 justify-start items-center text-blue-500 underline">
-                        <Image src={gitIcon} alt="git icon" width={20}/>
-                        github.com/numi8462
-                    </a>
-                </div>
+                
                 
                     
             </div>
