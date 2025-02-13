3a7d5ca7caf3ec2f9a5a596c4af58585fd25532a
numi8462 <yhk8462@naver.com>
2025-02-13 19:14:58 +0900

    Update image size for page

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: [
-            "prod-files-secure.s3.us-west-2.amazonaws.com",
-            "github.com",
-            "raw.githubusercontent.com",
-            // 다른 도메인을 여기에 추가하세요
-        ],
-    },
+  images: {
+    domains: [
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
+      "github.com",
+      "raw.githubusercontent.com",
+      "img1.daumcdn.net",
+      "velog.velcdn.com",
+      // 다른 도메인을 여기에 추가하세요
+    ],
+  },
 };
 
 export default nextConfig;
