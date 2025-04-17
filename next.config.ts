a80c2f60e055dd1afb574aaea57f1ef8b98cc7e2
numi8462 <yhk8462@naver.com>
2025-04-17 20:04:42 +0900

    refactor: next.config 수정

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,26 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      'prod-files-secure.s3.us-west-2.amazonaws.com',
-      'github.com',
-      'raw.githubusercontent.com',
-      'img1.daumcdn.net',
-      'velog.velcdn.com',
-      'tsh.io',
-      'private-user-images.githubusercontent.com',
-      // 다른 도메인을 여기에 추가하세요
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
+    // domains: [
+    //   'prod-files-secure.s3.us-west-2.amazonaws.com',
+    //   'github.com',
+    //   'raw.githubusercontent.com',
+    //   'img1.daumcdn.net',
+    //   'velog.velcdn.com',
+    //   'tsh.io',
+    //   'private-user-images.githubusercontent.com',
+    //   // 다른 도메인을 여기에 추가하세요
+    // ],
   },
 };
 
